fb157029e106a5b9185e26c13367bd72f12afbfa
gitsergio99 <chernikov1981sv@gmail.com>
2024-12-06 21:07:37 +0700

    modified:   threadtest.exe 	modified:   threadtest.nim

--- threadtest.nim
+++ threadtest.nim
@@ -1,25 +1,27 @@
 import taskpools
-import std/[random,strformat,os]
+import std/[sequtils,strutils,strformat,bitops,asyncnet, asyncdispatch,net,os]
+import mbtcpdevicefull
 
 var
     shared_var:int = 0
+    plc1:ModBus_Device
 
-randomize()
+plc1.modbus_adr=1
+var p:ptr[ModBus_Device] = plc1.addr
 
-proc random_task(n:int) =
-    
-    while true:
-        shared_var = rand(n)
-        echo fmt"second task {shared_var}"
-        sleep(1000)
+proc random_task() =
+    asyncCheck run_srv_asynch(p,502)
+    runForever()
 
 proc main_task() =
     var
         #ntreads = countProcessors()
+        hr:seq[int16] = @[]
         tp = Taskpool.new(num_threads = 4)
-    spawn(tp,random_task(100))
+    spawn(tp,random_task())
     while true:
-        echo fmt"main loop {shared_var}"
+        hr = plc1.hregs.gets(0,30)
+        echo fmt"curren state is {hr}"
         sleep(2000)
 
 main_task()
